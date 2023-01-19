633d8f6f97a4c74b8c72c06c168dc85e394e1cf9
ZiFuuuuuu <72281284+ZiFuuuuuu@users.noreply.github.com>
2023-01-19 11:58:43 +0800

    Update hwnat-boost-1G.sh

--- hwnat-boost-1G.sh
+++ hwnat-boost-1G.sh
@@ -10,5 +10,4 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
 #
-wget -c https://raw.githubusercontent.com/ZiFuuuuuu/actions-openwrt/main/322-mt7621-fix-cpu-clk-add-clkdev.patch
 cp -r ./322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10
