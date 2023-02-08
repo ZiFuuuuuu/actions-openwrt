71104d12f02396f4d9eb09b099dab6d00844a27f
ZiFuuuuuu <72281284+ZiFuuuuuu@users.noreply.github.com>
2023-02-09 03:27:23 +0800

    Update Modify.sh

--- diy-sh/Modify.sh
+++ diy-sh/Modify.sh
@@ -12,6 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-#
-#复制5.10内核超频补丁文件
-cp extra-files/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/
