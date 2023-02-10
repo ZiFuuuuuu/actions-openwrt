699bd175b5c76f720b8e67b612e8df91eaacd883
ZiFuuuuuu <72281284+ZiFuuuuuu@users.noreply.github.com>
2023-02-10 08:49:25 +0800

    Update Tools.sh

--- diy-sh/Tools.sh
+++ diy-sh/Tools.sh
@@ -11,3 +11,5 @@
 #
 # Add a feed source
 #echo 'src-git upx https://github.com/kuoruan/openwrt-upx' >>feeds.conf.default
+#echo 'src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+#echo 'src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
