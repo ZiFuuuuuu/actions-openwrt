eb23ac888f1fd4303ee4448367eeeed42d0d775d
ZiFuuuuuu <72281284+ZiFuuuuuu@users.noreply.github.com>
2025-12-10 09:44:57 +0800

    Update feed sources in Tools.sh
    Comment out existing feed sources and add a new one for nikki.

--- diy-sh/Tools.sh
+++ diy-sh/Tools.sh
@@ -10,6 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # Add a feed source
-echo 'src-git PWpackages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
-echo 'src-git PWluci https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
+#echo 'src-git PWpackages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+#echo 'src-git PWluci https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
 #echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.defaul
+echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >>feeds.conf.default
